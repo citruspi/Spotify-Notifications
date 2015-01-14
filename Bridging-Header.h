497926b7361d6d0776161f9a9ae60601c2964f44
Mihir Singh <git.service@mihirsingh.com>
2015-01-13 20:50:14 -0500

    Added the NSBundle+LoginItem category via CocoaPods

---
+++ Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Spotify-Notifications-Bridging-Header.h
+//  Spotify Notifications
+//
+//  Created by Mihir Singh on 1/13/15.
+//  Copyright (c) 2015 citruspi. All rights reserved.
+//
+
+#ifndef Spotify_Notifications_Spotify_Notifications_Bridging_Header_h
+#define Spotify_Notifications_Spotify_Notifications_Bridging_Header_h
+
+#import <NSBundle+LoginItem/NSbundle+LoginItem.h>
+
+#endif
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :osx
+
+pod 'NSBundle+LoginItem', '~> 1.0'
+

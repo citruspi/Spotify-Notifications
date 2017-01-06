c4dd8ea0fba69f82da6684e9a491a68458c799e1
Sebastian Jachec <sjachec1@gmail.com>
2017-01-06 17:45:43 +0000

    Re-ordered and renamed some Preferences items; updated Spotify scripting bridge file

--- Spotify Notifications/Spotify.swift
+++ Spotify Notifications/Spotify.swift
@@ -1,5 +1,4 @@
-import Cocoa
-import Foundation
+import AppKit
 import ScriptingBridge
 
 
@@ -26,23 +25,23 @@ import ScriptingBridge
     @objc optional var soundVolume: Int { get } // The sound output volume (0 = minimum, 100 = maximum)
     @objc optional var playerState: SpotifyEPlS { get } // Is Spotify stopped, paused, or playing?
     @objc optional var playerPosition: Double { get } // The player’s position within the currently playing track in seconds.
-    @objc optional var repeatingEnabled: Int { get } // Is repeating enabled in the current playback context?
-    @objc optional var repeating: Int { get } // Is repeating on or off?
-    @objc optional var shufflingEnabled: Int { get } // Is shuffling enabled in the current playback context?
-    @objc optional var shuffling: Int { get } // Is shuffling on or off?
+    @objc optional var repeatingEnabled: Bool { get } // Is repeating enabled in the current playback context?
+    @objc optional var repeating: Bool { get } // Is repeating on or off?
+    @objc optional var shufflingEnabled: Bool { get } // Is shuffling enabled in the current playback context?
+    @objc optional var shuffling: Bool { get } // Is shuffling on or off?
     @objc optional func nextTrack() // Skip to the next track.
     @objc optional func previousTrack() // Skip to the previous track.
     @objc optional func playpause() // Toggle play/pause.
     @objc optional func pause() // Pause playback.
     @objc optional func play() // Resume playback.
-    @objc optional func playTrack(_ x: Any!, inContext: Any!) // Start playback of a track in the given context.
+    @objc optional func playTrack(_ x: String!, inContext: String!) // Start playback of a track in the given context.
     @objc optional func setSoundVolume(_ soundVolume: Int) // The sound output volume (0 = minimum, 100 = maximum)
     @objc optional func setPlayerPosition(_ playerPosition: Double) // The player’s position within the currently playing track in seconds.
-    @objc optional func setRepeating(_ repeating: Int) // Is repeating on or off?
-    @objc optional func setShuffling(_ shuffling: Int) // Is shuffling on or off?
-    @objc optional var name: Int { get } // The name of the application.
-    @objc optional var frontmost: Int { get } // Is this the frontmost (active) application?
-    @objc optional var version: Int { get } // The version of the application.
+    @objc optional func setRepeating(_ repeating: Bool) // Is repeating on or off?
+    @objc optional func setShuffling(_ shuffling: Bool) // Is shuffling on or off?
+    @objc optional var name: String { get } // The name of the application.
+    @objc optional var frontmost: Bool { get } // Is this the frontmost (active) application?
+    @objc optional var version: String { get } // The version of the application.
 }
 
 extension SBApplication: SpotifyApplication {}
@@ -55,7 +54,7 @@ extension SBApplication: SpotifyApplication {}
     @objc optional var duration: Int { get } // The length of the track in seconds.
     @objc optional var playedCount: Int { get } // The number of times this track has been played.
     @objc optional var trackNumber: Int { get } // The index of the track in its album.
-    @objc optional var starred: Int { get } // Is the track starred?
+    @objc optional var starred: Bool { get } // Is the track starred?
     @objc optional var popularity: Int { get } // How popular is this track? 0-100
     @objc optional func id() -> String // The ID of the item.
     @objc optional var name: String { get } // The name of the track.
@@ -63,7 +62,7 @@ extension SBApplication: SpotifyApplication {}
     @objc optional var artwork: NSImage { get } // The property is deprecated and will never be set. Use the 'artwork url' instead.
     @objc optional var albumArtist: String { get } // That album artist of the track.
     @objc optional var spotifyUrl: String { get } // The URL of the track.
-    @objc optional func setSpotifyUrl(_ spotifyUrl: String) // The URL of the track.
+    @objc optional func setSpotifyUrl(_ spotifyUrl: String!) // The URL of the track.
 }
 extension SBObject: SpotifyTrack {}
 
